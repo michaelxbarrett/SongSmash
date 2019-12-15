f6150f94479d47d3ab483708517d8c0572245b2a
Michael Barrett <barrett@medium.com>
2019-12-15 14:23:17 -0800

    add image files

--- scripts/files.js
+++ scripts/files.js
@@ -1,38 +1,38 @@
 var InstrumentalFiles = [
     {
       name: "I'm Sprung",
-      file: 'assets/im_sprung_instrumental.wav',
-      img: 'https://c.saavncdn.com/720/Rappa-Ternt-Sanga-2006-500x500.jpg',
+      file: 'assets/audio/im_sprung_instrumental.wav',
+      img: 'assets/images/im-sprung-cover.jpg',
       bpm: 101
     },
     {
       name: "10 Crack Commandments",
-      file: 'assets/commandments_inst_87.mp3',
-      img: 'http://www.brooklynvegan.com/files/2019/05/notoriousbig.jpg?zc=1&s=0&a=t&q=89&w=630',
+      file: 'assets/audio/commandments_inst_87.mp3',
+      img: 'assets/images/biggie-cover.jpg',
       bpm: 87
     },
     {
       name: "I'm Sprung",
-      file: 'assets/money_grave_inst_101.mp3',
-      img: 'https://m.media-amazon.com/images/I/71NJFZme0nL._SS500_.jpg',
+      file: 'assets/audio/money_grave_inst_101.mp3',
+      img: 'assets/images/money-grave-cover.jpg',
       bpm: 101
     },
     {
       name: "I'm Sprung",
-      file: 'assets/doja_juicy_inst_132.mp3',
-      img: 'https://upload.wikimedia.org/wikipedia/en/thumb/9/90/Doja_Cat_and_Tyga_-_Juicy.png/220px-Doja_Cat_and_Tyga_-_Juicy.png',
+      file: 'assets/audio/doja_juicy_inst_132.mp3',
+      img: 'assets/images/juicy-cover.png',
       bpm: 132
     },
     {
       name: "I'm Sprung",
-      file: 'assets/go_loko_inst_101.mp3',
-      img: 'https://hiphop-n-more.com/wp-content/uploads/2019/05/go-loko-yg.jpg',
+      file: 'assets/audio/go_loko_inst_101.mp3',
+      img: 'assets/images/go-loko-yg.jpg',
       bpm: 101
     },
     {
       name: "I'm Sprung",
-      file: 'assets/pound_cake_inst_94.mp3',
-      img: 'https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/f7988ad8-ad4c-482f-8916-78aec9e2fc19/daem7sf-6f8f0ff4-6bb2-479b-8d50-da42dcff0bab.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcL2Y3OTg4YWQ4LWFkNGMtNDgyZi04OTE2LTc4YWVjOWUyZmMxOVwvZGFlbTdzZi02ZjhmMGZmNC02YmIyLTQ3OWItOGQ1MC1kYTQyZGNmZjBiYWIuanBnIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.raLfpQNuuIfKlyfu-PNSUd2Lhwes3cEuhOuiSgfce30',
+      file: 'assets/audio/pound_cake_inst_94.mp3',
+      img: 'assets/images/pound-cake-cover.jpg',
       bpm: 94
     }
   ]
@@ -40,37 +40,37 @@ var InstrumentalFiles = [
   var VocalFiles = [
     {
       name: 'Yes Indeed',
-      file: 'assets/better_now_vocals_145.mp3',
-      img: 'https://i.ytimg.com/vi/EvfQnhXY7Yk/maxresdefault.jpg',
+      file: 'assets/audio/better_now_vocals_145.mp3',
+      img: 'assets/images/better-now-cover.jpg',
       bpm: 145
     },
     {
       name: 'Quavo',
-      file: 'assets/quavo_working_vocals_170.mp3',
-      img: 'https://i.ytimg.com/vi/RiHPh2XLF8I/maxresdefault.jpg',
+      file: 'assets/audio/quavo_working_vocals_170.mp3',
+      img: 'assets/images/workin-e-cover.jpg',
       bpm: 170
     },
     {
       name: 'Unforgettable',
-      file: 'assets/unforgettable_vocals_98.mp3',
-      img: 'https://upload.wikimedia.org/wikipedia/en/d/d5/FrenchMontanaUnforgettable.jpg',
+      file: 'assets/audio/unforgettable_vocals_98.mp3',
+      img: 'assets/images/unforgettable-cover.jpg',
       bpm: 98
     },
     {
       name: 'Stallion',
-      file: 'assets/stallion_hotgirlsummer_acapella_163_34.mp3',
-      img: 'https://i.ytimg.com/vi/tS7cHwAmjV4/maxresdefault.jpg',
+      file: 'assets/audio/stallion_hotgirlsummer_acapella_163_34.mp3',
+      img: 'assets/images/hot-girl-summer.jpg',
       bpm: 81.67
     },
     {
       name: 'Old Town Road',
-      file: 'assets/old_town_road_acapella_132.mp3',
-      img: 'https://m.media-amazon.com/images/I/716IWHL37zL._SS500_.jpg',
+      file: 'assets/audio/old_town_road_acapella_132.mp3',
+      img: 'assets/images/old-town-road.jpg',
       bpm: 132
     },
     {
       name: 'Old Town Road',
-      file: 'assets/lizzo_juice_acapella_120.mp3',
+      file: 'assets/audio/lizzo_juice_acapella_120.mp3',
       img: 'https://static01.nyt.com/images/2019/04/25/arts/music/doas-lizzo-juice-thumbnail/doas-lizzo-juice-thumbnail-videoSixteenByNine768.png',
       bpm: 120
     }
