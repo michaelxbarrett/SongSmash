22e66316b51b816c5586b4ac87b4e0065934ad2a
Michael Barrett <barrett@medium.com>
2019-12-15 20:11:25 -0800

    use Fetch API

--- scripts/bufferloader.js
+++ scripts/bufferloader.js
@@ -8,16 +8,14 @@ function BufferLoader(context, urlList, callback) {
 
 BufferLoader.prototype.loadBuffer = function(url, index) {
   // Load buffer asynchronously
-  var request = new XMLHttpRequest();
-  request.open("POST", url, true);
-  request.responseType = "arraybuffer";
-
+  const request = new Request(url);
   var loader = this;
 
-  request.onload = function() {
-    // Asynchronously decode the audio file data in request.response
+  fetch(request)
+  .then(x => x.arrayBuffer())
+  .then(function(buffer){
     loader.context.decodeAudioData(
-      request.response,
+      buffer,
       function(buffer) {
         if (!buffer) {
           alert('error decoding file data: ' + url);
@@ -31,13 +29,38 @@ BufferLoader.prototype.loadBuffer = function(url, index) {
         console.error('decodeAudioData error', error);
       }
     );
-  }
+  })
 
-  request.onerror = function() {
-    alert('BufferLoader: XHR error');
-  }
+  // var request = new XMLHttpRequest();
+  // request.open("GET", url, true);
+  // request.responseType = "arraybuffer";
 
-  request.send();
+  // var loader = this;
+
+  // request.onload = function() {
+  //   // Asynchronously decode the audio file data in request.response
+  //   loader.context.decodeAudioData(
+  //     request.response,
+  //     function(buffer) {
+  //       if (!buffer) {
+  //         alert('error decoding file data: ' + url);
+  //         return;
+  //       }
+  //       loader.bufferList[index] = buffer;
+  //       if (++loader.loadCount == loader.urlList.length)
+  //         loader.onload(loader.bufferList);
+  //     },
+  //     function(error) {
+  //       console.error('decodeAudioData error', error);
+  //     }
+  //   );
+  // }
+
+  // request.onerror = function() {
+  //   alert('BufferLoader: XHR error');
+  // }
+
+  // request.send();
 }
 
 BufferLoader.prototype.load = function() {
